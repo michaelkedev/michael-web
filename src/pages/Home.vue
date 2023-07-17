638671c40154c5854df26961fb3730bfe25ef105
Michael Ke <k5855873@gmail.com>
2023-07-17 14:42:12 +0800

    change :  Change text color from stone to red on scroll

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -49,7 +49,7 @@
           v-bind="experience"
           :key="experience.text"
           class="text-stone-700"
-          :class="[scrollToMe(index) && ' text-stone-50']"
+          :class="[scrollToMe(index) && ' text-red-50']"
         />
       </div>
     </div>
